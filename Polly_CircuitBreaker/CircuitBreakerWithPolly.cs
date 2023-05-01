95900f9d979112c54c5b84a6d39878fb890e2c6b
Paul Thomas <paularun.prasath@gmail.com>
2023-05-01 09:02:31 +0400

    initial commit

---
+++ Polly_CircuitBreaker/CircuitBreakerWithPolly.cs
@@ -0,0 +1,24 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+
+HttpClient client = new HttpClient();
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://api.example.com/data");
+
+
+var circuitBreaker = Policy
+    .Handle<HttpRequestException>()
+    .CircuitBreakerAsync(
+        exceptionsAllowedBeforeBreaking: 2,
+        durationOfBreak: TimeSpan.FromSeconds(30)
+    );
+
+try
+{
+    HttpResponseMessage response = await circuitBreaker.ExecuteAsync(() => client.SendAsync(request));
+    string data = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(data);
+}
+catch (BrokenCircuitException)
+{
+    Console.WriteLine("The circuit is open - requests to the API are blocked.");
+}
